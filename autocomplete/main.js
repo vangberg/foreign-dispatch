f12973c1ebca18336c63554dabc4a941d6ae97cd
Harry Vangberg <harry@vangberg.name>
2022-11-17 12:22:03 +0100

    Autocompletion (#6)

--- autocomplete/main.js
+++ autocomplete/main.js
@@ -3,7 +3,7 @@ import initSqlJs from "sql.js";
 import sqlWasmUrl from "sql.js/dist/sql-wasm.wasm?url";
 import dbUrl from "./en-de.sqlite3?url";
 import { EditorView, basicSetup } from "codemirror";
-import { javascript } from "@codemirror/lang-javascript";
+import { autocompletion } from "@codemirror/autocomplete";
 
 async function main() {
   const sqlPromise = await initSqlJs({
@@ -15,27 +15,52 @@ async function main() {
   const [SQL, buf] = await Promise.all([sqlPromise, dataPromise]);
   const db = new SQL.Database(new Uint8Array(buf));
 
-  const stmt = db.prepare(`
-  SELECT written_rep, trans_list, sense_list
-  FROM (
-      SELECT DISTINCT written_rep
-      FROM search_trans
-      WHERE form MATCH :term
-    )
-    JOIN translation USING (written_rep)
-    ORDER BY
-      lower(written_rep) LIKE '%'|| lower(:term) ||'%' DESC, length(written_rep),
-      lexentry, coalesce(min_sense_num, '99'), importance * translation_score DESC
-    LIMIT 5
-  `);
-  stmt.bind({ ":term": "hello" });
-  while (stmt.step()) {
-    const row = stmt.getAsObject();
-    console.log(row);
+  function lookup(term) {
+    const stmt = db.prepare(`
+    SELECT written_rep, trans_list, sense_list
+    FROM (
+        SELECT DISTINCT written_rep
+        FROM search_trans
+        WHERE form MATCH :term
+      )
+      JOIN translation USING (written_rep)
+      ORDER BY
+        lower(written_rep) LIKE '%'|| lower(:term) ||'%' DESC, length(written_rep),
+        lexentry, coalesce(min_sense_num, '99'), importance * translation_score DESC
+      LIMIT 5
+    `);
+    stmt.bind({ ":term": `${term.slice(1)}*` });
+    const results = [];
+    while (stmt.step()) {
+      const row = stmt.getAsObject();
+      const translations = row.trans_list.split(" | ");
+      translations.forEach((translation) => {
+        results.push({
+          label: term,
+          type: "text",
+          detail: translation,
+          apply: translation,
+          info: row.sense_list,
+        });
+      });
+    }
+    return results;
+  }
+
+  function completions(context) {
+    let word = context.matchBefore(/@\w+/);
+    if (!word || (word.from == word.to && !context.explicit)) return null;
+    return {
+      from: word.from,
+      options: lookup(word.text),
+    };
   }
 
   let editor = new EditorView({
-    extensions: [basicSetup, javascript()],
+    extensions: [
+      basicSetup,
+      autocompletion({ override: [completions], activateOnTyping: true }),
+    ],
     parent: document.body,
   });
 }
